38eb4328164c2dc3237ff154a3a0505e8e04665e
Lukas Nielsen <nielsenlukas@web.de>
2023-02-07 13:13:24 +0100

    initial

---
+++ convert-easywk-to-json.php
@@ -0,0 +1,109 @@
+<?php
+
+function parseTime(string $i): float
+{
+    $callback = 0.0;
+
+    $i = array_reverse(preg_split("/[\.,:]/", $i));
+
+    if (isset($i[0])) {
+        $callback += (int) $i[0] / 100;
+    }
+
+    if (isset($i[1])) {
+        $callback += (int) $i[1];
+    }
+
+    if (isset($i[2])) {
+        $callback += (int) $i[2] * 60;
+    }
+
+    return round($callback, 2);
+}
+
+
+if (sizeof($argv) < 3) {
+    echo "Usage ..php {fina|master} {input filename}\n";
+    exit(1);
+}
+
+$MODE = $argv[1];
+$FILE = $argv[2];
+
+if (!file_exists($FILE)) {
+    echo "the given file does not exists\n";
+    exit(1);
+}
+
+$inputData = file_get_contents($FILE);
+
+$inputData = explode("\n", $inputData);
+
+$baseTimes = [];
+
+if (file_exists("./src/baseTimes.json")) {
+    $baseTimes = json_decode(file_get_contents("./src/baseTimes.json"), true);
+}
+
+foreach ($inputData as $line) {
+    $line = explode(";", $line);
+    switch ($MODE) {
+        case "fina":
+            if (sizeof($line) >= 8) {
+
+                $stroke = $line[4];
+
+                if ($line[3] === "4") {
+                    $stroke = "4x " . $stroke;
+                }
+
+                switch ($line[5]) {
+                    case "FREE":
+                        $stroke = $stroke . "F";
+                        break;
+                    case "FLY":
+                        $stroke = $stroke . "S";
+                        break;
+                    case "BREAST":
+                        $stroke = $stroke . "B";
+                        break;
+                    case "BACK":
+                        $stroke = $stroke . "R";
+                        break;
+                    case "MEDLEY":
+                        $stroke = $stroke . "L";
+                        break;
+                }
+
+                $year = $line[0];
+                $course = strtolower($line[1]);
+                $age = "all";
+                $gender = strtolower($line[2]);
+
+                $baseTimes[$MODE][$year][$course][$stroke][$age][$gender] = parseTime($line[6]);
+            }
+            break;
+        case "master":
+            if (sizeof($line) >= 6) {
+
+                $year = $line[4];
+                $course = strtolower($line[5]);
+                $age = $line[1];
+                $stroke = $line[0];
+
+                $baseTimes[$MODE][$year][$course][$stroke][$age]["m"] = parseTime($line[2]);
+                $baseTimes[$MODE][$year][$course][$stroke][$age]["f"] = parseTime($line[3]);
+            }
+            break;
+    }
+}
+
+if (isset($baseTimes["fina"])) {
+    ksort($baseTimes["fina"]);
+}
+if (isset($baseTimes["master"])) {
+    ksort($baseTimes["master"]);
+}
+
+
+file_put_contents("./src/baseTimes.json", json_encode($baseTimes, JSON_PRETTY_PRINT));
