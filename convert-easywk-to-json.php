dfbeba96e8b2d90bdeacabf4848fe76e83f0f9e2
Lukas Nielsen <moin@lukasnielsen.de>
2024-10-06 17:23:12 +0200

    update convert file

--- convert-easywk-to-json.php
+++ convert-easywk-to-json.php
@@ -2,37 +2,37 @@
 
 function parseTime(string $i): float
 {
-    $callback = 0.0;
+	$callback = 0.0;
 
-    $i = array_reverse(preg_split("/[\.,:]/", $i));
+	$i = array_reverse(preg_split("/[\.,:]/", $i));
 
-    if (isset($i[0])) {
-        $callback += (int) $i[0] / 100;
-    }
+	if (isset($i[0])) {
+		$callback += (int) $i[0] / 100;
+	}
 
-    if (isset($i[1])) {
-        $callback += (int) $i[1];
-    }
+	if (isset($i[1])) {
+		$callback += (int) $i[1];
+	}
 
-    if (isset($i[2])) {
-        $callback += (int) $i[2] * 60;
-    }
+	if (isset($i[2])) {
+		$callback += (int) $i[2] * 60;
+	}
 
-    return round($callback, 2);
+	return round($callback, 2);
 }
 
 
 if (sizeof($argv) < 3) {
 	echo "Usage ..php {wa|master} {input filename}\n";
-    exit(1);
+	exit(1);
 }
 
 $MODE = $argv[1];
 $FILE = $argv[2];
 
 if (!file_exists($FILE)) {
-    echo "the given file does not exists\n";
-    exit(1);
+	echo "the given file does not exists\n";
+	exit(1);
 }
 
 $inputData = file_get_contents($FILE);
@@ -42,67 +42,57 @@ $inputData = explode("\n", $inputData);
 $baseTimes = [];
 
 if (file_exists("./src/baseTimes.json")) {
-    $baseTimes = json_decode(file_get_contents("./src/baseTimes.json"), true);
+	$baseTimes = json_decode(file_get_contents("./src/baseTimes.json"), true);
 }
 
 foreach ($inputData as $line) {
-    $line = explode(";", $line);
-    switch ($MODE) {
+	$line = explode(";", $line);
+	switch ($MODE) {
 		case "wa":
-            if (sizeof($line) >= 8) {
+			if (sizeof($line) >= 8) {
 
-                $stroke = $line[4];
+				$stroke = $line[4];
 
-                if ($line[3] === "4") {
-                    $stroke = "4x " . $stroke;
-                }
+				if ($line[3] === "4") {
+					$stroke = "4x $stroke";
+				}
 
-                switch ($line[5]) {
-                    case "FREE":
-                        $stroke = $stroke . "F";
-                        break;
-                    case "FLY":
-                        $stroke = $stroke . "S";
-                        break;
-                    case "BREAST":
-                        $stroke = $stroke . "B";
-                        break;
-                    case "BACK":
-                        $stroke = $stroke . "R";
-                        break;
-                    case "MEDLEY":
-                        $stroke = $stroke . "L";
-                        break;
-                }
+				$stroke .= match ($line[5]) {
+					"FREE" => "F",
+					"FLY" => "S",
+					"BREAST" => "B",
+					"BACK" => "R",
+					"MEDLEY" => "L",
+				};
 
-                $year = $line[0];
-                $course = strtolower($line[1]);
-                $age = "all";
-                $gender = strtolower($line[2]);
+				$year = $line[0];
+				$course = strtolower($line[1]);
+				$age = "all";
+				$gender = strtolower($line[2]);
 
-                $baseTimes[$MODE][$year][$course][$stroke][$age][$gender] = parseTime($line[6]);
-            }
-            break;
-        case "master":
-            if (sizeof($line) >= 6) {
+				$baseTimes[$MODE][$year][$course][$stroke][$age][$gender] = parseTime($line[6]);
+			}
+			break;
+		case "master":
+			if (sizeof($line) >= 6) {
 
-                $year = $line[4];
-                $course = strtolower($line[5]);
-                $age = $line[1];
-                $stroke = $line[0];
+				$year = $line[4];
+				$course = strtolower($line[5]);
+				$age = $line[1];
+				$stroke = $line[0];
 
-                $baseTimes[$MODE][$year][$course][$stroke][$age]["m"] = parseTime($line[2]);
-                $baseTimes[$MODE][$year][$course][$stroke][$age]["f"] = parseTime($line[3]);
-            }
-            break;
-    }
+				$baseTimes[$MODE][$year][$course][$stroke][$age]["m"] = parseTime($line[2]);
+				$baseTimes[$MODE][$year][$course][$stroke][$age]["f"] = parseTime($line[3]);
+			}
+			break;
+	}
 }
 
 if (isset($baseTimes["wa"])) {
 	ksort($baseTimes["wa"]);
 }
 if (isset($baseTimes["master"])) {
-    ksort($baseTimes["master"]);
+	ksort($baseTimes["master"]);
 }
 
 
