b6aed57ed9b27e48c4a78c7b3c6d5e946c098beb
Lukas Nielsen <moin@lukasnielsen.de>
2026-03-07 16:27:20 +0100

    fix vulnerabilities

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 import oxlintPlugin from "vite-plugin-oxlint";
 
 export default defineConfig({
-	plugins: [
-		react(),
-		VitePWA({
-			registerType: "autoUpdate",
-			workbox: {
-				globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-			},
-		}),
-		oxlintPlugin(),
-	],
+	plugins: [react(), oxlintPlugin()],
 	resolve: {
 		alias: {
 			"@tabler/icons-react":
